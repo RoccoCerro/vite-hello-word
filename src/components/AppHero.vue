822f32ce8e01c10f6462375de3f4b3be8e9286a5
RoccoCerro <rcerro02@gmail.com>
2024-04-09 16:54:42 +0200

    Refactor

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="template-main">
+        <h1 class="title">Titolo Centrale</h1>
+    </div>
+</template>
+
+<style scoped>
+    .template-main {
+        height: calc(100vh - 57px);
+        width: 100%;
+        background-image: url(../../public/images1.jpg);
+        /* background-repeat: no-repeat; */
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-size: 100% 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title{
+        color: #B57269;
+        font-size: 100px;
+        font-weight: bold;
+        user-select: none ;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>La mia prima app con Vite</h1>
+</template>
+
+<style scoped>
+    h1{
+        text-align: center;
+        margin: 0;
+        padding: 10px;
+    }
+</style>
